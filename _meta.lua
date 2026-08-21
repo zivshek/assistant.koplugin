cdfce42ba1fb293bb30a45e3b46e440a94be3b62
zivshek <ziv.hao.shek@gmail.com>
2026-08-21 02:29:24 -0400

    Revert "feat: update version to 1.26 and implement stream continuation logic in Querier"
    This reverts commit 59080b5e21203079bcf13aebf1389dc07187cee8.

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("assistant_gettext")
 return {
     fullname = _("AI Assistant"),
     description = _("💡 Enhance your KOReader experience with AI"),
-    version = "1.26",
+    version = "1.25",
 }
